146c3d50cabe7f255b24e66315bb3bc9b671ee5c
SamuelMVieira <162640872+SamuelMVieira@users.noreply.github.com>
2024-05-13 20:24:29 -0300

    Add files via upload

---
+++ src/Exercicio_4.java
@@ -0,0 +1,16 @@
+public class Exercicio_4 {
+    public static void main(String[] args) {
+
+        System.out.println(">>> Listagem de Clientes");
+        System.out.println("________________________");
+        System.out.println("Nome: Fulano");
+        System.out.println("Data de Nascimento: 1980");
+        System.out.println("________________________");
+        System.out.println("Nome: Sicrano");
+        System.out.println("Data de Nascimento: 2000");
+
+
+
+    }
+
+}
